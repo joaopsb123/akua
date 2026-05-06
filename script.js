2ead974fb6aa2a72579e280f0596b2f3bf72ab4d
joaopsb123 <joaopsb12223@gmail.com>
2026-05-06 17:11:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ const CLIENT_ID = "1447995914290594056";
 const REDIRECT_URI = window.location.origin;
 
 const discordAuthURL =
-  `https://discord.com/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=identify`;
+  `https://discord.com/oauth2/authorize?client_id=1447995914290594056&response_type=code&redirect_uri=https%3A%2F%2Fakua-lake.vercel.app%2F&scope=identify`;
 
 document.getElementById("loginBtn").href = discordAuthURL;
